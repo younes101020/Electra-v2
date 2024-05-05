f1bd38339f383fa9446134752ac8730a35f1d21e
younes101020 <senyoupl@icloud.com>
2024-05-05 17:18:01 +0200

    first commit * init /accueil static content page

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex justify-center px-4 py-2">
+      <h1 className="text-4xl">Electra</h1>
+    </header>
+  );
+};
+
+export { Header };
--- src/app/accueil/page.tsx
+++ src/app/accueil/page.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import Image from "next/image";
 
-export default function Home() {
+export default function Accueil() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
