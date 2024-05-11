5bff5f1e3b8f0caedd538377972ac3d3ef2fb28a
younes101020 <senyoupl@icloud.com>
2024-05-11 18:34:33 +0200

    FINISH LANDING - [x] card integration - [x] add intensity to grayscale image

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="flex justify-center px-4 py-2">
-      <h1 className="text-4xl">Electra</h1>
+      <Link href="/">
+        <h1 className="text-4xl">Electra</h1>
+      </Link>
     </header>
   );
 };
--- src/app/accueil/page.tsx
+++ src/app/accueil/page.tsx
@@ -5,7 +5,7 @@ export default function Accueil() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
-        <Card>
+        <Card className="!relative">
           <CardContent className="!p-0">
             <Image
               src="https://i.pinimg.com/originals/b8/ee/9c/b8ee9c7cf0b95d728bf74f11df5e45cd.jpg"
@@ -14,7 +14,7 @@ export default function Accueil() {
               alt="Picture of the first position show"
             />
           </CardContent>
-          <CardFooter className="flex justify-between bg-background bg-opacity-50"></CardFooter>
+          <CardFooter className="flex justify-between bg-background/50 w-full bottom-0 absolute"></CardFooter>
         </Card>
       </section>
     </main>
