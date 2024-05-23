3117eaa204d1443bad6beb839ba61a1e0703a15a
younes101020 <senyoupl@icloud.com>
2024-05-24 00:43:14 +0200

    CHAT INTEGRATION

--- src/app/(protected)/space/[id]/page.tsx
+++ src/app/(protected)/space/[id]/page.tsx
@@ -1,3 +1,3 @@
-export default function SpacePage({ params }: { params: { id: string } }) {
+export default function CustomSpacePage({ params }: { params: { id: string } }) {
   return <div>Show space id: {params.id}</div>;
 }
